7edf9ff62379e46251c3442561ab1acec1bb54c5
Elroy Anaki <elroy.a1912@gmail.com>
2024-11-24 22:17:30 +0200

    code getAllProjects

--- server/controllers/project.controller.js
+++ server/controllers/project.controller.js
@@ -1,12 +1,20 @@
 const Project = require("../models/project.model");
 
-exports.getAllProjects = async (req, res) => {};
+exports.getAllProjects = async (req, res) => {
+  try {
+    const allProjects = await Project.find();
+    res.status(200).json({ succses: true, allProjects });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ succses: true, msg: error });
+  }
+};
 
 exports.getProjectById = async (req, res) => {};
 
 exports.createProject = async (req, res) => {
   try {
-    const newProject = Project.create(req.body);
+    const newProject = await Project.create(req.body);
     res.status(201).json({ succses: true, newProject });
   } catch (error) {
     res.status(500).json({ succses: true, msg: error });
